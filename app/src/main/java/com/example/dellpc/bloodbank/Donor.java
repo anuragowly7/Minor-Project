d43d29b2d915a27d9f3d30ee53516424b3abc159
Anurag Tamrakar <34151217+anuragowly7@users.noreply.github.com>
2018-07-28 22:47:22 +0545

    ..

--- app/src/main/java/com/example/dellpc/bloodbank/Donor.java
+++ app/src/main/java/com/example/dellpc/bloodbank/Donor.java
@@ -11,21 +11,12 @@ import android.widget.Button;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Donor extends AppCompatActivity {
-    Button logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_donor);
-        logout=(Button)findViewById(R.id.button7);
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                Intent i = new Intent(Donor.this,Homescreen.class);
-                startActivity(i);
-            }
-        });
+        setContentView(R.layout.activity_facts);
+
     }
 }
